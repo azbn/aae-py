44b2bb4bedef2505a8488825d03665e599a3ebd5
azbn <i@azbn.ru>
2017-10-10 07:10:52 +0300

    201710100710

---
+++ azbnpy.py
@@ -0,0 +1,44 @@
+#import sys
+
+class CreateAzbn(object):
+	
+	TITLE = 'Azbn Python Framework'
+	VERSION = '0.0.1'
+	
+	def __init__(self):
+		"""Init function"""
+		self._mdl = {}
+		self._data = {}
+	
+	def __call__(self, uid):
+		"""Get module for azbn"""
+		if uid in self._mdl:
+			return self._mdl[uid]
+		else:
+			return self
+	
+	def load(self, uid, obj):
+		"""Create module for azbn"""
+		self._mdl[uid] = obj
+		return self
+	
+	def __getitem__(self, uid):
+		"""Get item: azbn[]"""
+		if uid in self._data:
+			return self._data[uid]
+		else:
+			return None
+	
+	def __setitem__(self, uid, value):
+		"""Set item: azbn[]"""
+		self._data[uid] = value
+	
+	def info(self):
+		"""Echo TITLE and VERSION"""
+		print(self.TITLE, self.VERSION)
+	
+	def echo(self, s):
+		"""Echo string"""
+		print(s)
+		return self
+	
